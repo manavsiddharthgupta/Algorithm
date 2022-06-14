efc24e4be4cdbe98064b29dd57af61fe2658b45a
Manav Gupta <2006468@kiit.ac.in>
2022-06-14 22:57:46 +0530

    matrix

--- BinarySearch/question3.java
+++ BinarySearch/question3.java
@@ -1,10 +1,15 @@
 package com.manav.BinarySearch;
 
 public class question3 {
+    // 852. Peak Index in a Mountain Array - leetcode
+    // links - https://leetcode.com/problems/peak-index-in-a-mountain-array/
     public static void main(String[] args) {
         int[]  arr = {0, 1, 0};
         System.out.println(peakIndexInMountainArray(arr));
     }
+
+    // Time Complexity - O(LogN)
+    // Space Complexity - O(1)
     public static int peakIndexInMountainArray(int[] arr) {
         int start = 0;
         int end = arr.length-1;
